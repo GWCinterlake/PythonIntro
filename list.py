dcce0cb8116fd82627a164c5072ee1e465761d5c
GWCinterlake <56985412+GWCinterlake@users.noreply.github.com>
2019-10-24 20:02:41 -0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+WomenInTech = ["Ada Lovelace", "Miral Kotb", "Kim Swift", "Madison Maxey", "Esther Nam", "Marieme Jamme", "Theresa Johnson"]
+print(WomenInTech)
+print(len(WomenInTech))
+print("Bill Gates" in WomenInTech)
+print("Ada Lovelace" in WomenInTech)
+i=0
+for i in range(len(WomenInTech)):
+    print(WomenInTech[i])
+    i+=1
+for name in WomenInTech:
+        print(name)
+list = [["list 1 element 1", "list 1 element 2"], ["list 2 element 1", "list 2 element 2"]]
+print(list)
