05674242311a49e234446f67035eb772b9c1bb72
GWCinterlake <56985412+GWCinterlake@users.noreply.github.com>
2020-06-18 11:13:33 -0700

    update and add 0

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,4 +1,4 @@
-userPW=input("What is your password?")
+userPW=input("What is your password? ")
 error = 0
 if (len(userPW)<6)|(len(userPW)>16):
     print("Password invalid. Must be at least 6 characters long but no more than 16.")
@@ -9,8 +9,8 @@ numCount = 0
 symCount = 0
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 upperCase = ["A", "B", "C", "D", "E", "F", 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', "Y", "Z"]
-numCase = ['1','2','3','4','5','6','7','8','9']
-symCase=["@", "#", "!"]
+numCase = ['1','2','3','4','5','6','7','8','9', '0']
+symCase=["@", "#", "!", "'"]
 
 i=0
 for i in range(len(userPW)):
